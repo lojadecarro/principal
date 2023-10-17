2ab02c1933ee79197be61015ea8176d490f939df
Tro Armen <troarmen@gmail.com>
2023-10-16 23:35:14 -0300

    ModeloDAO

--- src/TabelasDao/Modelo/Principal.java
+++ src/TabelasDao/Modelo/Principal.java
@@ -7,18 +7,17 @@ import Tables.Modelo;
 
 public class Principal {
     public static void main(String[] args) throws SQLException {
+        
         ModeloDAO modeloDao = new ModeloDAO();
 
         MarcaDAO marcaDao = new MarcaDAO();
 
         CategoriaModeloDAO categoriaModeloDao = new CategoriaModeloDAO();
 
-        Modelo modelo = new Modelo(marcaDao.findById(2), categoriaModeloDao.findById(4), "testemodelo8");
+        Modelo modelo = new Modelo(marcaDao.findById(3), categoriaModeloDao.findById(5), "testemodelo9");
 
         Modelo modeloCriado = modeloDao.create(modelo);
 
         System.out.println(modeloCriado.getId());
-
-        System.out.println(modeloDao.findById(6));
     }
 }
